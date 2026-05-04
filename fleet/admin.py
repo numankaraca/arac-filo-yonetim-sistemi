7e470f38a0b48df90235bf65538f4bf7f127ecc1
numankaraca <sekercaytuz@gmail.com>
2026-05-04 14:59:35 +0300

    some changes on logo part

--- fleet/admin.py
+++ fleet/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Vehicle, Department, User, Document, Inspection, TrafficInsurance, CascoPolicy, MaintenanceRecord
 
-# Register your models here.
+admin.site.site_header = "İstanbul İl Göç İdaresi Araç Yönetimi"
+admin.site.site_title = "İl Göç İdaresi Admin"
+admin.site.index_title = "Araç Yönetim Sistemine Hoş Geldiniz"
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('plate', 'brand', 'model', 'department', 'status')
+    search_fields = ('plate', 'brand', 'model')
+
+admin.site.register(Department)
+admin.site.register(User)
+admin.site.register(Document)
+admin.site.register(Inspection)
+admin.site.register(TrafficInsurance)
+admin.site.register(CascoPolicy)
+admin.site.register(MaintenanceRecord)
